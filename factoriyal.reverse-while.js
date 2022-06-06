dea795e16bcfbb72732909baa67fc646947379e8
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-06 12:35:20 +0600

    four

--- factoriyal.reverse-while.js
+++ factoriyal.reverse-while.js
@@ -16,3 +16,54 @@ function reverse(num) {
 const myfactoriyal = reverse(5);
 
 console.log(myfactoriyal);
+
+
+
+
+
+
+function reverse(num) {
+
+    let factorial = 1;
+
+    let i = num;
+
+    while (i >= 1); {
+
+        factorial = factorial * i;
+        i--;
+    }
+
+    return factorial;
+}
+
+const myyyfactoriyal = reverse(3);
+
+console.log(myyyfactoriyal);
+
+
+
+
+
+
+
+
+
+function reverse(num) {
+
+    let factorial = 1;
+
+    let i = num;
+
+    while (i >= 1); {
+
+        factorial = factorial * i;
+        i--;
+    }
+
+    return factorial;
+}
+
+const myyfactoriyal = reverse(6);
+
+console.log(myyfactoriyal);
