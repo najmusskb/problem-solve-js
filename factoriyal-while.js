1a300b02621257dcee0c2c9c52e69f2a7ecfb2c8
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-06 12:15:56 +0600

    second

--- factoriyal-while.js
+++ factoriyal-while.js
@@ -12,8 +12,183 @@ function getfactorial(number) {
     return factorial;
 }
 
-const myFactoriyal = 5;
-const result = getfactorial(myFactoriyal); {
+const firstFactoriyal = 2;
+const result = getfactorial(firstFactoriyal); {
 
     console.log("this is the first while factorial:", result);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const second = 6;
+const result2 = getfactorial(second); {
+
+    console.log("this is the first while factorial:", result);
+}
+
+
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+
+
+const thrd = 7;
+const ressult = getfactorial(thrd); {
+
+    console.log("this is the first while factorial:", ressult);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const foreth = 3;
+const resultT = getfactorial(foreth); {
+
+    console.log("this is the first while factorial:", resultT);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const five = 16;
+const resuLt = getfactorial(five); {
+
+    console.log("this is the first while factorial:", resuLt);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const six = 5;
+const resUlt = getfactorial(six); {
+
+    console.log("this is the first while factorial:", resUlt);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const seven = 9;
+const rEsult = getfactorial(seven); {
+
+    console.log("this is the first while factorial:", rEsult);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const eight = 10;
+const Result = getfactorial(eight); {
+
+    console.log("this is the first while factorial:", Result);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const nine = 11;
+const reSult = getfactorial(nine); {
+
+    console.log("this is the first while factorial:", reSult);
+}
+function getfactorial(number) {
+
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        factorial = factorial * i;
+        i++;
+
+
+
+    }
+    return factorial;
+}
+
+const ten = 12;
+const resultt = getfactorial(ten); {
+
+    console.log("this is the first while factorial:", resultt);
 }
